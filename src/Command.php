3142785dbd5d4d22f5155cf4ebe70f000abf62d0
Edo Barnas <iam@edobarnas.com>
2022-12-15 11:59:46 +0100

    Change the return value of the ID getter in the `Command` class

--- src/Command.php
+++ src/Command.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Termyn\Cqrs;
 
 use DateTimeImmutable as DateTime;
+use Termyn\Identifier\Gid;
 
 interface Command extends Message
 {
+    public function id(): Gid;
+
     public function issuedOn(): DateTime;
 }
