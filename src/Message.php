26ceb25e3c491356c6227a3e0ad47f525f52a752
Edo Barnas <iam@edobarnas.com>
2022-12-19 20:46:01 +0100

    Use `GlobalId` in a message

--- src/Message.php
+++ src/Message.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Termyn\Cqrs;
 
-use Termyn\Identifier\Id;
+use Termyn\GlobalId;
 
 interface Message
 {
-    public function id(): Id;
+    public function id(): GlobalId;
 }
